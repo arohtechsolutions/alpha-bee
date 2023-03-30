9ea2753b10014c7b256e5c30dbc6b1d1cce9830a
arohtechsolutions <126889472+arohtechsolutions@users.noreply.github.com>
2023-03-30 22:18:28 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<header class="header d-flex flex-row">
+		<div class="header_content d-flex flex-row align-items-center">
+			<!-- Logo -->
+			<div class="logo_container">
+				<div class="logo">
+					<img src="images/akcalogo.png" alt="">
+					<span>AKCA</span>
+				</div>
+			</div>
+
+			<!-- Main Navigation -->
+			<nav class="main_nav_container">
+				<div class="main_nav">
+					<ul class="main_nav_list">
+						<li class="main_nav_item"><a href="#">home</a></li>
+						<li class="main_nav_item"><a href="#">about us</a></li>
+						<li class="main_nav_item"><a href="courses.html">courses</a></li>
+						<li class="main_nav_item"><a href="elements.html">Gallery</a></li>
+						<li class="main_nav_item"><a href="news.html">news</a></li>
+						<li class="main_nav_item"><a href="contact.html">contact</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+		<div class="header_side d-flex flex-row justify-content-center align-items-center">
+			<img src="images/phone-call.svg" alt="">
+			<span><a href="tel:phone-call">+91 7020487909</span>
+		</div>
+
+		<!-- Hamburger -->
+		<div class="hamburger_container">
+			<i class="fas fa-bars trans_200"></i>
+		</div>
+
+	</header>
